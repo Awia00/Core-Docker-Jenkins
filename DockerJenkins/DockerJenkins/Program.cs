5672e0b11a8b899e0c19e8796ba52641beea239c
Anders Wind <awis@itu.dk>
2017-08-10 15:21:54 +0200

    trying some more

--- DockerJenkins/DockerJenkins/Program.cs
+++ DockerJenkins/DockerJenkins/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace DockerJenkins
 {
@@ -11,6 +12,7 @@ namespace DockerJenkins
     {
         public static void Main(string[] args)
         {
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
